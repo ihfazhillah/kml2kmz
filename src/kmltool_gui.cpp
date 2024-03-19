6eb852fc993dd9b2c24e1737f2609ba183cec681
ihfazhillah <mihfazhillah@gmail.com>
2024-03-19 23:16:49 +0700

    tambah simple validasi

--- src/kmltool_gui.cpp
+++ src/kmltool_gui.cpp
@@ -48,13 +48,33 @@ KMLToolController::KMLToolController(MainFrame * view){
 
 void KMLToolController::OnSubmit(wxCommandEvent &event){
     std::string label = this->view->text_label->GetValue().ToStdString();
+
+    if (label.empty())
+    {
+        wxMessageBox("Label tidak boleh kosong.", "Error", wxOK | wxICON_ERROR, this->view);
+        return;
+    }
+
     std::string output_dir = this->view->m_dir_output->GetTextCtrlValue().ToStdString();
+    if (output_dir.empty())
+    {
+        wxMessageBox("Output directory tidak boleh kosong.", "Error", wxOK | wxICON_ERROR, this->view);
+        return;
+    }
+
     int notebook_selection = this->view->m_notebook1->GetSelection();
 
     if (notebook_selection == 0)
     {
+
         // kml -> kmz
         std::string kml_path = this->view->m_kml_file->GetTextCtrlValue().ToStdString();
+        if (kml_path.empty())
+        {
+            wxMessageBox("KML file tidak boleh kosong.", "Error", wxOK | wxICON_ERROR, this->view);
+            return;
+        }
+
         coordinates coordinate_items = get_coordinates(kml_path);
         mkdir(label.c_str(), 0777);
         for (int i = 0; i < coordinate_items.size; i++)
@@ -65,24 +85,27 @@ void KMLToolController::OnSubmit(wxCommandEvent &event){
                 compiled_coordinate,
                 label,
                 output_dir,
-                i
-            );
+                i);
         }
-        
-    } else {
+    }
+    else
+    {
         // coordinate -> kmz
 
         int index = 0;
         std::string coordinate = this->view->m_koordinat_text->GetValue().ToStdString();
+        if (coordinate.empty())
+        {
+            wxMessageBox("coordinate tidak boleh kosong.", "Error", wxOK | wxICON_ERROR, this->view);
+            return;
+        }
         save_coordinate(
             coordinate,
             label,
             output_dir,
-            index
-        );
+            index);
     }
 
-        wxMessageBox("Konversi Sukses",
+    wxMessageBox("Konversi Sukses",
                  "Sukses", wxOK | wxICON_INFORMATION, this->view);
-
 }
